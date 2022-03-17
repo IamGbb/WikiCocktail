07a30a7d77bd8ba935d8b0de998199fb4f7a761c
Gilad <giladmta@gmail.com>
2022-03-17 11:59:40 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about</h1>
+      <p>
+        Boker tov olam, ma shlom kulam? ani yored me ha hutim.
+      </p>
+    </section>
+  )
+}
+
+export default About
